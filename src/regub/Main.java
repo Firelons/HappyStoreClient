658e15967b3d7752352cf8c0fb8c7789f0521f08
erjius <antoinespielmann@yahoo.fr>
2015-04-01 15:34:38 +0200

    Ajout de la classe principale du programme

---
+++ src/regub/Main.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package regub;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * Classe représentant l'application
+ * 
+ * @author antoine
+ */
+public class Main extends Application {
+
+    private final double MINIMUM_WINDOW_WIDTH = 640;
+    private final double MINIMUM_WINDOW_HEIGHT = 480;
+
+    private Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            stage = primaryStage;
+            stage.setTitle("Regub");
+            stage.setMinWidth(MINIMUM_WINDOW_WIDTH);
+            stage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
+            stage.setScene(new Scene(new Pane()));
+            stage.centerOnScreen();
+            primaryStage.show();
+    //////////   PAGE D'ACCUEIL   ////////////
+            gotoPage("Boutton1");  //TODO : remplacer par la page de login
+    //////////////////////////////////////////
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+    /**
+     * charge le fxml et change de page
+     * 
+     * @param pageName nom du fxml, sans l'extension
+     */
+    public void gotoPage(String pageName) {
+        FXMLLoader loader = new FXMLLoader();
+        Pane page;
+        try (InputStream in = Main.class.getResourceAsStream(pageName + ".fxml")) {
+            loader.setBuilderFactory(new JavaFXBuilderFactory());
+            loader.setLocation(Main.class.getResource(pageName + ".fxml"));
+            page = loader.load(in);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+        AbstractController ac = (AbstractController) loader.getController();
+        ac.setApp(this);
+        Scene scene = new Scene(page,
+                stage.isShowing() ? stage.getScene().getWidth() : MINIMUM_WINDOW_WIDTH,
+                stage.isShowing() ? stage.getScene().getHeight() : MINIMUM_WINDOW_HEIGHT);
+        stage.setScene(scene);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
