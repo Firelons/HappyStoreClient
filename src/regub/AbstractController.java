96d9eb5e9811db260e11fb8bbec1f37d6815844e
erjius <antoinespielmann@yahoo.fr>
2015-04-01 15:37:31 +0200

    Ajout d'une classe de base pour les controleur

---
+++ src/regub/AbstractController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regub;
+
+import javafx.fxml.Initializable;
+
+/**
+ * Classe de base de tous les controleurs
+ *
+ * @author antoine
+ */
+public abstract class AbstractController implements Initializable {
+
+    private Main appMain;
+
+    /**
+     * Donne au controleur l'address de l'instance Main (appelée automatiquement
+     * lors du gotoPage()), doit être surchargée en cas d'inclusion de
+     * controleur et lancer sur chaque controleur inclus.
+     *
+     * @param main
+     */
+    public void setApp(Main main) {
+        appMain = main;
+    }
+
+    /**
+     * Permet l'accès à l'instance de Main (Application) affin par exemple de
+     * changer de page
+     *
+     * @return l'objet Main
+     */
+    public Main getApp() {
+        return appMain;
+    }
+}
