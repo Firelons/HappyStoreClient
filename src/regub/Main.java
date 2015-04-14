f785ad5372e232398d710d1e10a3ac3ca059873c
erjius <antoinespielmann@yahoo.fr>
2015-04-14 16:12:56 +0200

    chargement de la page de login par default

--- src/regub/Main.java
+++ src/regub/Main.java
@@ -38,7 +38,7 @@ public class Main extends Application {
             stage.centerOnScreen();
             primaryStage.show();
     //////////   PAGE D'ACCUEIL   ////////////
-            gotoPage("Boutton1");  //TODO : remplacer par la page de login
+            gotoPage("login");  //TODO : remplacer par la page de login
     //////////////////////////////////////////
         } catch (Exception ex) {
             System.err.println(ex.getMessage());
