641071dfd30cd432c04d48ecf3b1fcd907d2438d
Mesmerus <Mesmerus@192.168.1.32>
2015-04-14 14:36:59 +0200

    Le macquettes concernant le commercial sont maintenant bien intégrées au projet

--- src/regub/commercial/AccueilCommercialController.java
+++ src/regub/commercial/AccueilCommercialController.java
@@ -10,38 +10,27 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import regub.AbstractController;
 
 /**
  *
  * @author Mesmerus
  */
-public class AccueilCommercialController implements Initializable {
+public class AccueilCommercialController extends AbstractController {
 
     @FXML
     private void AjouterContrat(ActionEvent event) throws IOException {
-        Parent PageAccueilParent = FXMLLoader.load(getClass().getResource("Contrat.fxml"));
-        Scene PageAccueilScene = new Scene(PageAccueilParent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(PageAccueilScene);
-        stage.show();
+        getApp().gotoPage("commercial/Contrat");
     }
+
     @FXML
     private void AjouterClient(ActionEvent event) throws IOException {
-        Parent PageAccueilParent = FXMLLoader.load(getClass().getResource("Client.fxml"));
-        Scene PageAccueilScene = new Scene(PageAccueilParent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(PageAccueilScene);
-        stage.show();
+        getApp().gotoPage("commercial/Client");
     }
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 
 }
--- src/regub/commercial/ContratController.java
+++ src/regub/commercial/ContratController.java
@@ -10,28 +10,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import regub.AbstractController;
 
 /**
  * FXML Controller class
  *
  * @author Mesmerus
  */
-public class ContratController implements Initializable {
+public class ContratController extends AbstractController {
 
     @FXML
     private void Annuler(ActionEvent event) throws IOException {
-        Parent PageAccueilParent = FXMLLoader.load(getClass().getResource("AccueilCommercial.fxml"));
-        Scene PageAccueilScene = new Scene(PageAccueilParent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        //stage.hide();
-        stage.setScene(PageAccueilScene);
-        stage.show();
+       getApp().gotoPage("commercial/AccueilCommercial");
     }
 
     @Override
