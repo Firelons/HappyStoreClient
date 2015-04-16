415cfb4de9b82a5d4657c6baf808f19e4682d491
Lons <Lons@10.248.50.31>
2015-04-16 21:34:45 +0200

    Insertion Test dans la base de données

--- src/regub/commercial/ClientController.java
+++ src/regub/commercial/ClientController.java
@@ -7,6 +7,10 @@ package regub.commercial;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,9 +24,39 @@ public class ClientController extends AbstractController{
 
     @FXML
     private void Annuler(ActionEvent event) throws IOException {
+        this.Save_Client("test");
         getApp().gotoPage("commercial/AccueilCommercial");
+        
     }
-
+    @FXML
+    private void Save_Client(String Client) throws IOException {
+        String url = "jdbc:mysql://localhost/regub";
+        String login="com";
+        String passwd ="com";
+        Connection cn=null;
+        Statement st=null;
+        int Num=4;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = DriverManager.getConnection(url, login, passwd);
+            st=cn.createStatement();
+            String sql ="INSERT INTO client(idClient,societe,telephone,email,addr_ligne1,addr_ligne2,ville,code_postal) VALUES (3,'TF2',0601,'lons@lons.fr','zozo','zozo','loin',8700)";
+            st.executeUpdate(sql);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                cn.close();
+                st.close();
+            }catch (SQLException e){
+            e.printStackTrace();
+        }
+        }
+        
+       
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
