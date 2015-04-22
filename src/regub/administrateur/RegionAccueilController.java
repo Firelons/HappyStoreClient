3dc62715b56421d132a64497c3cfd26d57fbe0a3
erjius <antoinespielmann@yahoo.fr>
2015-04-22 20:08:19 +0200

    integration de la barre utilisateur

--- src/regub/administrateur/RegionAccueilController.java
+++ src/regub/administrateur/RegionAccueilController.java
@@ -7,32 +7,35 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import regub.AbstractController;
+import regub.Main;
+import regub.util.UserBarController;
 
-public class RegionAccueilController extends AbstractController{
-        
-	
-	@FXML
-    private TextField NomRegion ;
-	
-	@FXML
-           private void RegionAjouter(ActionEvent event) {
-            getApp().gotoPage("administrateur/RegionAJMOSU");
-        }
-           
-	@FXML
-           private void RegionModifier(ActionEvent event) {
-            getApp().gotoPage("administrateur/RegionAJMOSU");
-        }
-        
-        
+public class RegionAccueilController extends AbstractController {
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-            
-	}
+    @FXML
+    private TextField NomRegion;
+
+    @FXML
+    private UserBarController  usermenuController;
+
+    @FXML
+    private void RegionAjouter(ActionEvent event) {
+        getApp().gotoPage("administrateur/RegionAJMOSU");
+    }
+
+    @FXML
+    private void RegionModifier(ActionEvent event) {
+        getApp().gotoPage("administrateur/RegionAJMOSU");
+    }
+
+    @Override
+    public void setApp(Main m) {
+        super.setApp(m);
+        usermenuController.setApp(m);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
-	
-	
-
-
-
