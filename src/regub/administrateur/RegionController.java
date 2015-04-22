9ee44e560966eda1b59d228981edbb15c3f19ae9
Souad <Souad@192.168.1.35>
2015-04-22 16:19:26 +0200

    admin!!

---
+++ src/regub/administrateur/RegionController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regub.administrateur;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import regub.AbstractController;
+
+/**
+ *
+ * @author Souad
+ */
+public class RegionController extends AbstractController{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+  
+    }
+    
+}
