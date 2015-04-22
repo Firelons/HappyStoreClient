9215dfd00750454a7cd771fadfa29a6a53125025
erjius <antoinespielmann@yahoo.fr>
2015-04-22 13:19:01 +0200

    rename

---
+++ src/regub/administrateur/AccueilAdminController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regub.administrateur;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author admin
+ */
+public class AccueilAdminController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    }    
+}
