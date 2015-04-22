e75a8b5755a703db768e6484f62946ed1d94c794
erjius <antoinespielmann@yahoo.fr>
2015-04-22 14:05:27 +0200

    surcharge de setApp

---
+++ src/regub/administrateur/AccueilAdministrateurController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regub.administrateur;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import regub.AbstractController;
+import regub.Main;
+import regub.util.UserBarController;
+
+/**
+ * FXML Controller class
+ *
+ * @author admin
+ */
+public class AccueilAdministrateurController extends AbstractController{
+
+    
+    @FXML
+    private UserBarController usermenuController;
+    
+    @Override
+    public void setApp(Main m){
+        super.setApp(m);
+        usermenuController.setApp(m);
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    }    
+}
