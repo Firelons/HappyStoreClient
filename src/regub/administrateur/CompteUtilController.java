117098f97aa406d5387a837458bcba27190147b3
erjius <antoinespielmann@yahoo.fr>
2015-04-22 20:08:08 +0200

    integration de la barre utilisateur

--- src/regub/administrateur/CompteUtilController.java
+++ src/regub/administrateur/CompteUtilController.java
@@ -1,14 +1,24 @@
-
 package regub.administrateur;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import regub.AbstractController;
-
+import regub.Main;
+import regub.util.UserBarController;
 
 public class CompteUtilController extends AbstractController {
+
+    @FXML
+    private UserBarController usermenuController;
+
+    @Override
+    public void setApp(Main m) {
+        super.setApp(m);
+        usermenuController.setApp(m);
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-  
     }
 }
