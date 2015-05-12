98db65cae172a44bc4c02138d5241233a4831a86
Souad <Souad@192.168.1.18>
2015-05-12 10:44:31 +0200

    admin vac !!

--- src/regub/administrateur/RegionAccueilController.java
+++ src/regub/administrateur/RegionAccueilController.java
@@ -1,12 +1,20 @@
 package regub.administrateur;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import regub.AbstractController;
+import regub.Auth;
 import regub.Main;
 import regub.util.UserBarController;
 
@@ -17,6 +25,12 @@ public class RegionAccueilController extends AbstractController {
 
     @FXML
     private UserBarController  usermenuController;
+    
+    @FXML
+    private ObservableList<String> regionData = FXCollections.observableArrayList();
+    
+    @FXML
+    private ResultSet rsRegion;
 
     @FXML
     private void RegionAjouter(ActionEvent event) {
@@ -28,11 +42,44 @@ public class RegionAccueilController extends AbstractController {
         getApp().gotoPage("administrateur/RegionAJMOSU");
     }
 
+         @FXML
+    private void getRegionDB() throws IOException {
+        
+        System.out.println(Auth.getUserInfo().toString());
+
+        try (Connection cn = Auth.getConnection();
+                Statement st = cn.createStatement()) {
+            String sql = "SELECT * FROM region";
+
+            rsRegion=st.executeQuery(sql);
+            while(rsRegion.next()){
+                regionData.add(new String(rsRegion.getString("libelle")));
+               
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+          
+    }
+    public ObservableList<String> getRegionData()  {
+        
+        try {
+            this.getRegionDB();
+        } catch (IOException ex) {
+             ex.printStackTrace();
+        }
+         regionData.add(new String("4")) ;
+         return regionData;
+    }
+    
+    
     @Override
     public void setApp(Main m) {
         super.setApp(m);
         usermenuController.setApp(m);
     }
+    
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
