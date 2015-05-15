c842a80a733dd20a27525f00de1a353c5bef2355
erjius <antoinespielmann@yahoo.fr>
2015-05-15 17:10:27 +0200

    gestion mise à jour util

--- src/regub/administrateur/CompteUtilController.java
+++ src/regub/administrateur/CompteUtilController.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,12 +18,10 @@ import javafx.scene.control.TableView;
 import regub.AbstractController;
 import regub.Auth;
 import regub.Main;
-import regub.commercial.Client;
 import regub.util.UserBarController;
 
 public class CompteUtilController extends AbstractController {
-    
-    
+
     //Proprietés pour l'utilisateur
     private final ObservableList<Utilisateur> UtilData = FXCollections.observableArrayList();
     @FXML
@@ -35,84 +34,81 @@ public class CompteUtilController extends AbstractController {
     private TableColumn<Utilisateur, String> Typecmpt;
     @FXML
     private TableColumn<Utilisateur, String> login;
-    
-    
-     //boutons de la page
+
+    //boutons de la page
     @FXML
     private Button AjouterUtil;
     @FXML
     private Button ModifierUtil;
     @FXML
     private Button SupprimerUtil;
-    
-    private ResultSet rsUtil;//Récupère la liste des clients dans la base de donées
 
     @FXML
     private UserBarController usermenuController;
-    
+
     @FXML
     private void CompteUtilAjouter(ActionEvent event) {
         getApp().gotoPage("administrateur/CompteAJMO");
     }
-    
-     @FXML
-    private void CompteUtilModifier(ActionEvent event) {
-        
-        Utilisateur.setCurUtil(UtilTable.getSelectionModel().getSelectedItem());
-        
-        getApp().gotoPage("administrateur/CompteAJMO");
-    }
-    
-     @FXML
-    private void CompteUtilSupprimer(ActionEvent event) {
-        
-       
-        
-        getApp().gotoPage("administrateur/CompteAJMO");
-    }
-    
-    
-     @FXML
-     private void getUtilDB() throws IOException{
-         
-      try (Connection cn = Auth.getConnection();
-                Statement st = cn.createStatement()) {
-            String sql = "SELECT * FROM Compte inner join TypeCompte on Compte.idTypeCompte=TypeCompte.idTypeCompte";
 
-            rsUtil = st.executeQuery(sql);
-            System.out.println(rsUtil);
-            while (rsUtil.next()) {
-                UtilData.add(new Utilisateur(rsUtil.getInt("idCompte"), rsUtil.getString("nom"), rsUtil.getString("prenom"),
-                rsUtil.getString("login"), rsUtil.getString("libelle") ));
+    @FXML
+    private void CompteUtilModifier(ActionEvent event) {
+        getApp().gotoPage("administrateur/CompteAJMO");
+    }
+
+    @FXML
+    private void getUtilDB() throws IOException {
+
+        String sql = "SELECT idCompte ,nom ,prenom ,login ,libelle "
+                + "FROM Compte inner join TypeCompte ON Compte.idTypeCompte=TypeCompte.idTypeCompte";
+        try (
+                Connection cn = Auth.getConnection();
+                Statement st = cn.createStatement();
+                ResultSet res = st.executeQuery(sql)) {
+
+            while (res.next()) {
+                UtilData.add(
+                        new Utilisateur(
+                                res.getInt("idCompte"), res.getString("nom"),
+                                res.getString("prenom"), res.getString("login"),
+                                res.getString("libelle")
+                        )
+                );
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-     }
-     public ObservableList<Utilisateur>getUtilData(){
-          try {
-            this.getUtilDB();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        return UtilData;
-        }
-     
-   
+    }
 
     @Override
     public void setApp(Main m) {
         super.setApp(m);
         usermenuController.setApp(m);
-         UtilTable.setItems(getUtilData());
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Utilisateur.setCurUtil(null);
+        try {
+            getUtilDB();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        UtilTable.setItems(UtilData);
         nom.setCellValueFactory(cellData -> cellData.getValue().getNom());
         prenom.setCellValueFactory(cellData -> cellData.getValue().getPrenom());
         Typecmpt.setCellValueFactory(cellData -> cellData.getValue().gettype_compte());
         login.setCellValueFactory(cellData -> cellData.getValue().getLogin());
-     }
+        UtilTable.getSelectionModel().getSelectedItems().addListener((ListChangeListener.Change<? extends Utilisateur> c) -> {
+            while (c.next()) {
+                if (c.wasAdded()) {
+                    Utilisateur.setCurUtil(c.getAddedSubList().get(0));
+                    ModifierUtil.setDisable(false);
+                    SupprimerUtil.setDisable(false);
+                }
+            }
+        });
+    }
+
 }
