e3ba23ca577f46325ff80c4e576ca083788c41e0
Lons <Lons@Lons-PC>
2015-11-02 13:00:43 +0100

    Rajout des fonctionnalités de reporting Devis

--- src/regub/commercial/ContratController.java
+++ src/regub/commercial/ContratController.java
@@ -5,6 +5,15 @@
  */
 package regub.commercial;
 
+import javax.swing.table.DefaultTableModel;
+import java.util.HashMap;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRTableModelDataSource;
+import net.sf.jasperreports.view.JasperViewer;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -96,6 +105,8 @@ public class ContratController extends AbstractController {
     @FXML
     private Button save;
     @FXML
+    private Button devis;
+    @FXML
     private RadioButton valide;
     @FXML
     private RadioButton preparation;
@@ -120,6 +131,8 @@ public class ContratController extends AbstractController {
 
     @FXML
     private ToggleGroup etatContrat;
+    
+    DefaultTableModel tableModel;
 
     private HashMap<String, Integer> RegionData;
     private HashMap<String, Integer> RayonData;
@@ -130,6 +143,41 @@ public class ContratController extends AbstractController {
     private void Annuler(ActionEvent event) throws IOException {
         Video.setCurVideo(null);
         getApp().gotoPage("commercial/AccueilCommercial");
+    }
+    
+    @FXML
+    private void Consulter(ActionEvent event) throws IOException {
+        JasperPrint jasperPrint = null;
+        net.sf.jasperreports.engine.JasperReport x = null; 
+        TableModelData();
+        try {
+            x = JasperCompileManager.compileReport("reports/devis.jrxml");
+            //JasperCompileManager.compileReportToFile("reports/report1.jrxml");
+            jasperPrint = JasperFillManager.fillReport(x, new HashMap(),
+                    new JRTableModelDataSource(tableModel));
+            JasperViewer jasperViewer = new JasperViewer(jasperPrint);
+            jasperViewer.setVisible(true);
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+         private void TableModelData() {
+        String[] columnNames = {"Id", "Name", "Department", "Email"};
+        String[][] data = {
+            {"111", "G Conger", " Orthopaedic", "jim@wheremail.com"},
+            {"222", "A Date", "ENT", "adate@somemail.com"},
+            {"333", "R Linz", "Paedriatics", "rlinz@heremail.com"},
+            {"444", "V Sethi", "Nephrology", "vsethi@whomail.com"},
+            {"555", "K Rao", "Orthopaedics", "krao@whatmail.com"},
+            {"666", "V Santana", "Nephrology", "vsan@whenmail.com"},
+            {"777", "J Pollock", "Nephrology", "jpol@domail.com"},
+            {"888", "H David", "Nephrology", "hdavid@donemail.com"},
+            {"999", "P Patel", "Nephrology", "ppatel@gomail.com"},
+            {"101", "C Comer", "Nephrology", "ccomer@whymail.com"}
+        };
+        tableModel = new DefaultTableModel(data,columnNames);
+    
     }
 
     @FXML
