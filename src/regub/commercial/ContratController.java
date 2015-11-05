923575a72783743afd62c9886bf04883e8e606f6
projet <projet@projet-ThinkCentre-M55>
2015-11-05 16:34:10 +0100

    Entete client Devis

--- src/regub/commercial/ContratController.java
+++ src/regub/commercial/ContratController.java
@@ -163,18 +163,17 @@ public class ContratController extends AbstractController {
     }
     
          private void TableModelData() {
-        String[] columnNames = {"Id", "Name", "Department", "Email"};
+             
+             
+        String[] columnNames = {"Nom", "Adresse", "Code", "Ville","Numéro","Mail"};
         String[][] data = {
-            {"111", "G Conger", " Orthopaedic", "jim@wheremail.com"},
-            {"222", "A Date", "ENT", "adate@somemail.com"},
-            {"333", "R Linz", "Paedriatics", "rlinz@heremail.com"},
-            {"444", "V Sethi", "Nephrology", "vsethi@whomail.com"},
-            {"555", "K Rao", "Orthopaedics", "krao@whatmail.com"},
-            {"666", "V Santana", "Nephrology", "vsan@whenmail.com"},
-            {"777", "J Pollock", "Nephrology", "jpol@domail.com"},
-            {"888", "H David", "Nephrology", "hdavid@donemail.com"},
-            {"999", "P Patel", "Nephrology", "ppatel@gomail.com"},
-            {"101", "C Comer", "Nephrology", "ccomer@whymail.com"}
+            {Client.getCurClient().getSociete(),
+             Client.getCurClient().getRue(), 
+             Client.getCurClient().getPostalCode(), 
+             Client.getCurClient().getVille(),
+             Client.getCurClient().getTelephone(), 
+            Client.getCurClient().getEmail()},
+            
         };
         tableModel = new DefaultTableModel(data,columnNames);
     
